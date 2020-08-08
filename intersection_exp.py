a80d183ebd71216047a0e675f76c6bf3f409d1fb
mfatihaktas <mfatihaktas@gmail.com>
2020-08-08 00:41:54 -0400

    Untabify

--- intersection_exp.py
+++ intersection_exp.py
@@ -4,39 +4,39 @@ from sim_objs import *
 from intersection_sim import *
 
 def plot_EN_vs_Delta_in_TimeMix():
-	def plot_(n):
-		log(INFO, ">> n= {}".format(n) )
-		Delta_l = []
-		EN_sim_l = []
-		for Delta in np.linspace(1, 5, 5):
-			print("Delta= {}".format(Delta) )
-			Delta_l.append(Delta)
+  def plot_(n):
+    log(INFO, ">> n= {}".format(n) )
+    Delta_l = []
+    EN_sim_l = []
+    for Delta in np.linspace(1, 5, 5):
+      print("Delta= {}".format(Delta) )
+      Delta_l.append(Delta)
 
-			T = TPareto(0.1, Delta, 1)
-			EN_sim = sim_EN_TimeMix(n, T, num_sim_runs=1000)
-			print("EN_sim= {}".format(EN_sim))
-			EN_sim_l.append(EN_sim)
+      T = TPareto(0.1, Delta, 1)
+      EN_sim = sim_EN_TimeMix(n, T, num_sim_runs=1000)
+      print("EN_sim= {}".format(EN_sim))
+      EN_sim_l.append(EN_sim)
 
-		c = next(darkcolor_c)
-		plot.plot(Delta_l, EN_sim_l, label=r'$n= {}$'.format(n), color=c, marker=next(marker_c), mew=mew, ms=ms, linestyle=':')
-		# plot.yscale('log')
+    c = next(darkcolor_c)
+    plot.plot(Delta_l, EN_sim_l, label=r'$n= {}$'.format(n), color=c, marker=next(marker_c), mew=mew, ms=ms, linestyle=':')
+    # plot.yscale('log')
 
-	for n in range(10, 40, 10):
+  for n in range(10, 40, 10):
     plot_(n)
   
   plot.legend()
-	plot.xlabel(r'$\Delta$', fontsize=14)
-	plot.ylabel(r'$E[N]$', fontsize=14)
-	plot.title(r'Time-Mix')
-	plot.gcf().set_size_inches(6, 5)
-	plot.savefig("plot_EN_vs_Delta_in_TimeMix.pdf", bbox_inches='tight')
-	log(WARNING, "done.")
+  plot.xlabel(r'$\Delta$', fontsize=14)
+  plot.ylabel(r'$E[N]$', fontsize=14)
+  plot.title(r'Time-Mix')
+  plot.gcf().set_size_inches(6, 5)
+  plot.savefig("plot_EN_vs_Delta_in_TimeMix.pdf", bbox_inches='tight')
+  log(WARNING, "done.")
 
 if __name__ == "__main__":
-	plot_EN_vs_Delta_in_TimeMix()
+  plot_EN_vs_Delta_in_TimeMix()
 
-	# n = 10
-	# T = TPareto(0.1, 5, 2)
-	# EN_sim = sim_EN_TimeMix(n, T, num_sim_runs=1)
-	# print("EN_sim= {}".format(EN_sim))
+  # n = 10
+  # T = TPareto(0.1, 5, 2)
+  # EN_sim = sim_EN_TimeMix(n, T, num_sim_runs=1)
+  # print("EN_sim= {}".format(EN_sim))
 
